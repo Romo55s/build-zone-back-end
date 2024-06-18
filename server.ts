7626e8b68c6743b5d991995ebbbf32e52d02ef2e
Tony <pp.tony777@gmail.com>
2024-06-18 07:19:47 -0600

    Adding  0.0.0.0

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
-const port = process.env.PORT || 10000;
+const port = Number(process.env.PORT) || 10000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
