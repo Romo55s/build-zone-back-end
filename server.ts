a0d2f2c8146d4794ddff89e1afda2e75074bbf05
Tony <pp.tony777@gmail.com>
2024-06-18 06:31:09 -0600

    Chaning the port

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
